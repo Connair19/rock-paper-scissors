456abbfa8e78ee1cb212f3434e6218811d326bc4
Connair19 <10air.ozr@gmail.com>
2022-03-21 18:18:03 +0300

    game function completed

--- script.js
+++ script.js
@@ -12,22 +12,20 @@ function computerSelect() {
           :move <= 33 ? "Rock"
           :"Paper"
 }
-let computerMove = computerSelect();
 
 /* Ask for a move */
 
 function playerSelect() {
    let move = prompt("Enter your move:");
-   move = move.charAt(0).toUpperCase() + move.slice(1).toLowerCase();
+   move = capitalize(move);
    /* Check if the move is valid and if it's not valid ask again with the name of the moves */
-   move = checkValid(move)
+   move = checkValid(move);
    return move
 }
-let playerMove = playerSelect();
 
 /* Compare the user's move with the computer move and return the result */
 function checkWinner(computerMove, playerMove) {
-
+    console.log("Computer chooses:" + computerMove)
     return computerMove === "Rock" && playerMove === "Paper" ? "Player"
          : computerMove === "Rock" && playerMove === "Scissors" ? "Computer"
          : computerMove === "Paper" && playerMove === "Rock" ? "Computer"
@@ -37,16 +35,68 @@ function checkWinner(computerMove, playerMove) {
          : "Tie";
      }
 
-let winner = checkWinner(computerMove,playerMove)
-console.log(computerMove)
-console.log(playerMove)
-console.log(winner)
+function playRound() {
+    let computerMove = computerSelect();
+    let playerMove = playerSelect();
+    let winner = checkWinner(computerMove, playerMove);
+    if (winner === "Computer") {
+        console.log("You lose!" + computerMove + " beats " + playerMove + "!")
+    }
+    else if (winner === "Player") {
+        console.log("You win!" + playerMove + " beats " + computerMove + "!")
+    }
+    else {
+        console.log("It's a tie!.")
+    }
+    return winner
+}
+
+
+function capitalize(string) {
+    string = string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    return string
+}
 
 function checkValid(move) {
     while(moves.includes(move) === false) {
         move = prompt("Please enter a move which can be rock, paper or scissors.")
-        move = move.charAt(0).toUpperCase() + move.slice(1).toLowerCase();
+        move = capitalize(move);
         }
     return move
 }
+
+function game() {
+    let computerWins = 0;
+    let playerWins = 0;
+    for(let i = 0; i < 5; i++) {
+        winner = playRound()
+        if(winner === "Computer") {computerWins++}
+        else if(winner === "Player") {playerWins++}
+    }
+    console.log("Player: " + playerWins)
+    console.log("Computer: " + computerWins)
+    if (playerWins > computerWins) {console.log("Congratulations! You beat the computer!")}
+    else if (playerWins < computerWins) {console.log("That's unlucky, you lost to computer.")}
+    else {console.log("It's a tie. You almost made it!")}
+}
+
+function playAgain() {
+    let answer = prompt("Do you want to play again?");
+    answer = capitalize(answer);
+    console.log(answer);
+    while (answer !== "Yes" && answer !== "No") {
+        answer = prompt("Please enter yes or no.")
+    } 
+    if (answer === "Yes") { return true}
+    else if(answer === "No") {return false}
+    
+}
+
+
+game()
+while (playAgain()) {
+    game()
+}
+
+
 
